77d8889fc71ea9e40cc1dcc54848bacd07dde07b
kimi <hr6594@naver.com>
2024-11-14 14:03:23 +0900

    이미지 파일 경로 변경 및 마무리

--- 20241111_02_severvingrobots/objects.py
+++ 20241111_02_severvingrobots/objects.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import os
 
 class Robot:
     def __init__(self, canvas):
         self.canvas = canvas
-        self.robots_image = Image.open("robots.png")
+        self.robots_image = Image.open(os.path.join("20241111_02_severvingrobots", "images", "robots.png"))
         self.robots_image = self.robots_image.resize((100, 100))
         self.robot_image = ImageTk.PhotoImage(self.robots_image)
         self.robot = self.canvas.create_image(400, 270, anchor="center", image=self.robot_image)  # self.robot에 저장
@@ -14,7 +15,7 @@ class Bell:
     def __init__(self, canvas, bell_id):
         self.canvas = canvas
         self.id = bell_id
-        self.bells_image = Image.open("bell.png")
+        self.bells_image = Image.open(os.path.join("20241111_02_severvingrobots", "images", "bell.png"))
         self.bells_image = self.bells_image.resize((60, 60))
         self.bell_image = ImageTk.PhotoImage(self.bells_image)
         self.bell = self.canvas.create_image(50, 50, anchor="center", image=self.bell_image)
